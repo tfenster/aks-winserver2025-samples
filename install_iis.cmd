d748ce89de9e79be8aad70ab890e78abe604da08
Tobias Fenster <tfenster@4psbau.de>
2025-12-07 13:33:39 +0100

    Add script to install IIS with reboot handling

---
+++ install_iis.cmd
@@ -0,0 +1,9 @@
+REM FoDs such as Microsoft.NanoServer.IIS will trigger a request for reboot.
+REM For those FoDs, DISM will exit with 3010 which must be handled to prevent the batch from exiting with a non-zero status.
+REM Additionally we must supply the /NoRestart argument to suppress the reboot prompt.
+DISM /Online /Add-Capability /CapabilityName:Microsoft.NanoServer.IIS /NoRestart
+if errorlevel 3010 (
+    echo The specified optional feature requested a reboot which was suppressed.
+    exit /b 0
+)
+@echo off
